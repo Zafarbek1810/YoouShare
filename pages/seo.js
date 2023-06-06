f383f46972f77c4bce33cb091e65577ebc642984
Zafarbek1810 <zafarbekyoldoshev1810@gmail.com>
2023-06-06 10:11:28 +0500

    img changed seo

--- pages/seo.js
+++ pages/seo.js
@@ -12,14 +12,14 @@ export default function SEO() {
             <meta property="og:url" content="https://metatags.io/" />
             <meta property="og:title" content="Yoou Share" />
             <meta property="og:description" content="Почасовая аренда Power Bank"/>
-            <meta property="og:image" content="https://metatags.io/assets/meta-tags-16a33a6a8531e519cc0936fbba0ad904e52d35f34a46c97a2c9f6f7dd7d336f2.png" />
+            <meta property="og:image" content="../public/images/seo.jpg" />
 
 
             <meta property="twitter:card" content="summary_large_image" />
             <meta property="twitter:url" content="https://metatags.io/" />
             <meta property="twitter:title" content="Yoou Share" />
             <meta property="twitter:description" content="Почасовая аренда Power Bank"/>
-            <meta property="twitter:image" content="https://metatags.io/assets/meta-tags-16a33a6a8531e519cc0936fbba0ad904e52d35f34a46c97a2c9f6f7dd7d336f2.png" />
+            <meta property="twitter:image" content="../public/images/seo.jpg" />
         </Head>
     )
 }
