cf0d6dab76901995e67b5f2337fe6e01f40d0421
Zafarbek1810 <zafarbekyoldoshev1810@gmail.com>
2023-06-12 17:52:38 +0500

    seo change img

--- pages/contacts.js
+++ pages/contacts.js
@@ -10,6 +10,18 @@ export default function Home() {
         <title>Yoou Share</title>
         <meta name="description" content="Возможность оставаться на связи 24 часа в сутки!" />
         <link rel="icon" href="/favicon.ico" />
+        <meta name="og:type" content="website" />
+            <meta name="og:url" content="https://metatags.io/" />
+            <meta name="og:title" content="Yoou Share" />
+            <meta name="og:description" content="Возможность оставаться на связи 24 часа в сутки!"/>
+            <meta name="og:image" content="https://i.ibb.co/cQC6Xj4/seo.jpg" />
+
+
+            <meta name="twitter:card" content="summary_large_image" />
+            <meta name="twitter:url" content="https://metatags.io/" />
+            <meta name="twitter:title" content="Yoou Share" />
+            <meta name="twitter:description" content="Возможность оставаться на связи 24 часа в сутки!"/>
+            <meta name="twitter:image" content="https://i.ibb.co/cQC6Xj4/seo.jpg" />
       </Head>
       <Contacts />
       <SEO/>
--- pages/seo.js
+++ pages/seo.js
@@ -8,18 +8,18 @@ export default function SEO() {
             <meta name="title" content="Yoou Share" />
             <meta name="description" content="Почасовая аренда Power Bank"/>
 
-            <meta property="og:type" content="website" />
-            <meta property="og:url" content="https://metatags.io/" />
-            <meta property="og:title" content="Yoou Share" />
-            <meta property="og:description" content="Почасовая аренда Power Bank"/>
-            <meta property="og:image" content="../public/images/seo.jpg" />
+            <meta name="og:type" content="website" />
+            <meta name="og:url" content="https://metatags.io/" />
+            <meta name="og:title" content="Yoou Share" />
+            <meta name="og:description" content="Почасовая аренда Power Bank"/>
+            <meta name="og:image" content="https://i.ibb.co/cQC6Xj4/seo.jpg" />
 
 
-            <meta property="twitter:card" content="summary_large_image" />
-            <meta property="twitter:url" content="https://metatags.io/" />
-            <meta property="twitter:title" content="Yoou Share" />
-            <meta property="twitter:description" content="Почасовая аренда Power Bank"/>
-            <meta property="twitter:image" content="../public/images/seo.jpg" />
+            <meta name="twitter:card" content="summary_large_image" />
+            <meta name="twitter:url" content="https://metatags.io/" />
+            <meta name="twitter:title" content="Yoou Share" />
+            <meta name="twitter:description" content="Почасовая аренда Power Bank"/>
+            <meta name="twitter:image" content="https://i.ibb.co/cQC6Xj4/seo.jpg" />
         </Head>
     )
 }
