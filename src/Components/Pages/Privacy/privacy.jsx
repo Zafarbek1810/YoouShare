01d8b9414735681b23f5aa0bf79ece0465302a8d
Samandar Eshmamatov <samandareshmamatov1127@gmail.com>
2025-05-06 18:57:46 +0500

    Update the contents of public offer page

--- src/Components/Pages/Privacy/privacy.jsx
+++ src/Components/Pages/Privacy/privacy.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { useTranslation } from "react-i18next";
+import { Trans, useTranslation } from "react-i18next";
 import Container from "../../Common/Container/index";
 import PrivacyWrapper from "./privacy.style";
 
@@ -9,7 +9,30 @@ const Privacy = () => {
   return (
     <PrivacyWrapper>
       <Container>
-        <p>{t("privacy_text")}</p>
+        <p>
+          <Trans
+            i18nKey="privacy.new_text"
+            components={{
+              title: <h3 style={{ fontSize: "20px", textAlign: "center" }} />,
+              link1: (
+                <a
+                  href="https://yoou.uz"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  style={{ color: "blue", textDecoration: "underline" }}
+                />
+              ),
+              link2: (
+                <a
+                  href="https://yoou.uz/privacy"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  style={{ color: "blue", textDecoration: "underline" }}
+                />
+              ),
+            }}
+          />
+        </p>
         {/* <p>{t("privacy.text")}</p>
         <p>{t("privacy.text2")}</p>
         <p>{t("privacy.text3")}</p> */}
