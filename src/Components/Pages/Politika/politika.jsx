5ac720ed257843d0110d78d6bc4ee890c2c6dc2a
Samandar Eshmamatov <samandareshmamatov1127@gmail.com>
2025-05-06 15:12:28 +0500

    Update privacy page contents

--- src/Components/Pages/Politika/politika.jsx
+++ src/Components/Pages/Politika/politika.jsx
@@ -1,15 +1,49 @@
 import React from "react";
-import { useTranslation } from "react-i18next";
-import Container from "../../Common/Container/index"
+import { Trans, useTranslation } from "react-i18next";
+import Container from "../../Common/Container/index";
 import PolitikaWrapper from "./politika.style";
 
 const Politika = () => {
   const { t } = useTranslation();
 
+  const text = t("politika.new_text", {
+    link1: `<a href="https://yoou.uz" target="_blank" rel="noopener noreferrer">https://yoou.uz</a>`,
+  });
+
   return (
     <PolitikaWrapper>
       <Container>
-        <p>{t("politika.text2")}</p>
+        <p>
+          <Trans
+            i18nKey="politika.new_text"
+            components={{
+              1: (
+                <a
+                  href="https://yoou.uz"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  style={{ color: "blue", textDecoration: "underline" }}
+                />
+              ),
+              2: (
+                <a
+                  href="mailto:info@yoou.uz"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  style={{ color: "blue", textDecoration: "underline" }}
+                />
+              ),
+              3: (
+                <a
+                  href="https://yoou.uz/privacy"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  style={{ color: "blue", textDecoration: "underline" }}
+                />
+              ),
+            }}
+          />
+        </p>
       </Container>
     </PolitikaWrapper>
   );
